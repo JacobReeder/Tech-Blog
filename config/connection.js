79fc3dea660b092e62daa0cc930deba06f69e30b
JacobReeder <jacob.reeder1@gmail.com>
2022-06-25 16:48:51 -0700

    Added date

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 
 
-/*let sequelize;
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -14,12 +14,12 @@ if (process.env.JAWSDB_URL) {
     dialect: 'mysql',
     port: 3306
   });
-}*/
+}
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+/*const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
-});
+});*/
 
 module.exports = sequelize;
