960cc555fa647abff41f50c96e0cf1b28e3c9370
JacobReeder <jacob.reeder1@gmail.com>
2022-06-26 10:19:19 -0700

    Added dashboard routes, functionality, and styles

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,11 +3,17 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('homepage', {
+  loggedIn: req.session.loggedIn,
+});
 });
 
 router.get('/login', (req, res) => {
-    res.render('login');
-  });
+  if (req.session.loggedIn) { // if already logged in. Add logout first before uncommenting
+    res.redirect('/dashboard');
+    return;
+  }
+  res.render('login');
+});
 
 module.exports = router;
